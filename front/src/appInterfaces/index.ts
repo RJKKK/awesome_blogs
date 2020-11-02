28950acceb509f98ddf571c60abf994bda72a4bd
RJKKK <shengunkun@gmail.com>
2020-11-03 07:57:16 +0800

    添加组件参数接口

---
+++ front/src/appInterfaces/index.ts
@@ -0,0 +1,22 @@
+export enum AssemblyType {
+    image,
+    text,
+    sticker
+}
+interface Assembly {
+    id:string
+    componentType:AssemblyType
+    stickerCenterX:number
+    stickerCenterY:number
+    zIndex:number
+    scale:number
+    rotate:number
+    stickerId?:string
+    text?:string
+    imageUrl?:string
+    whScale?:number
+}   
+export interface Journal {
+    backgroundId:string|number
+    Assemblies:Assembly[]
+}
