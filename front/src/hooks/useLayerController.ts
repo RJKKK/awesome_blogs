7961cdc979b749dcf8dae40ce12307596977065f
RJKKK <shengunkun@gmail.com>
2020-11-25 11:58:22 +0800

    逻辑修改

--- front/src/hooks/useLayerController.ts
+++ front/src/hooks/useLayerController.ts
@@ -1,36 +1,46 @@
 import * as fabricjs from 'fabric'
 // @ts-ignore
 const fabric = fabricjs.default.fabric;
-import {onMounted, nextTick, ref} from 'vue'
-import {Object} from "fabric/fabric-impl";
+import {onMounted, nextTick, ref,Ref,reactive} from 'vue'
+import {Object,Image} from "fabric/fabric-impl";
 
-export function useLayerController(element: HTMLElement, jsonContent?: Object) {
-    const canvas = ref<fabric.Canvas>(null)
+export function useLayerController(element: Ref<HTMLElement>, jsonContent?: Object) {
+    let canvas:fabric.Canvas = null
     const clipboard = ref<Object[]>([])
-    const context = canvas.value
     const addText = (text: string = "") => {
         const newText = new fabric.IText(text)
-        context.add(newText)
+        canvas.add(newText)
     }
     const addImage = (url: string) => {
-        const newImage = new fabric.Image.fromURL(url)
-        context.add(newImage)
+        fabric.Image.fromURL(url,(img:Image)=>{
+            img.set({
+                left: 100, // 图片相对画布的左侧距离
+                top: 100, // 图片相对画布的顶部距离
+                opacity: 0.85, // 图片透明度
+                // 这里可以通过scaleX和scaleY来设置图片绘制后的大小，这里为原来大小的一半
+                scaleX: 0.5,
+                scaleY: 0.5,
+                lockRotation:false,
+            });
+            canvas.add(img)
+        })
+
 
     }
     const hide = () => {
-        context.getActiveObject().set('opacity', 0)
-        context.requestRenderAll()
+        canvas.getActiveObject().set('opacity', 0)
+        canvas.requestRenderAll()
     }
     const display = () => {
-        context.getActiveObject().set('opacity', 1)
-        context.requestRenderAll()
+        canvas.getActiveObject().set('opacity', 1)
+        canvas.requestRenderAll()
     }
     const setClipboard = () => {
-        const copyObjects: Object[] = context.getActiveObjects()
+        const copyObjects: Object[] = canvas.getActiveObjects()
         clipboard.value = copyObjects
     }
     const paste = () => {
-        context.discardActiveObject()
+        canvas.discardActiveObject()
         clipboard.value.forEach(val => {
             val.clone(clone => {
                 clone.set({
@@ -38,20 +48,25 @@ export function useLayerController(element: HTMLElement, jsonContent?: Object) {
                     top: clone.top + 20,
                     evented: true
                 } as Partial<Object>)
-                context.add(clone)
+                canvas.add(clone)
                 val.left += 20;
                 val.top += 20;
-                context.setActiveObject(clone)
+                canvas.setActiveObject(clone)
             })
         })
     }
     const del = ()=>{
-        const delObjects:Object[] = context.getActiveObjects()
-        delObjects.forEach(val=>context.remove(val))
+        const delObjects:Object[] = canvas.getActiveObjects()
+        delObjects.forEach(val=>canvas.remove(val))
     }
-    onMounted(() => {
-        canvas.value = fabric.Canvas(element.id)
+    nextTick(() => {
+        // console.log(document.getElementById('canvas'))
+        canvas = new fabric.Canvas(element.value)
+        console.log(canvas)
         if (jsonContent)
-            canvas.value.loadFromJSON(jsonContent, () => {})
+            canvas.loadFromJSON(jsonContent, () => {})
     })
+    return {
+        addText,addImage,del,hide,display,setClipboard,paste
+    }
 }
--- front/src/views/testdemo.vue
+++ front/src/views/testdemo.vue
@@ -1,32 +1,19 @@
 <template>
-    <canvas id="canvas" width="800" height="800"></canvas>
+    <canvas  ref="canvasElement" width="800" height="800"></canvas>
 </template>
 
 <script lang="ts">
-    import {defineComponent,onMounted} from "vue";
-    import {fabric} from '../untils/fabricjs'
-    import {Image} from "fabric/fabric-impl";
+    import {defineComponent,onMounted,ref,onUpdated,nextTick} from "vue";
+    import {useLayerController} from '../hooks/useLayerController'
     export default defineComponent({
         setup(){
-            console.log(fabric)
-            onMounted(()=>{
-                const canvas =  new fabric.Canvas('canvas');
-                let text = new fabric.IText('hello world', { left: 100, top: 100 });
-                canvas.add(text);
-                fabric.Image.fromURL('src/assets/logo.png', (img:Image) => {
-                    img.set({
-                        left: 100, // 图片相对画布的左侧距离
-                        top: 100, // 图片相对画布的顶部距离
-                        angle: 30, // 图片旋转角度
-                        opacity: 0.85, // 图片透明度
-                        // 这里可以通过scaleX和scaleY来设置图片绘制后的大小，这里为原来大小的一半
-                        scaleX: 0.5,
-                        scaleY: 0.5
-                    });
-                    // 添加对象
-                    canvas.add(img);
-                });
+            const canvasElement = ref<HTMLElement>(null)
+            const {addText,addImage,del,hide,display,setClipboard,paste } = useLayerController(canvasElement)
+            nextTick(()=>{
+                addImage('https://api.r10086.com/CG系列1.php')
+                addText('aabb')
             })
+            return{canvasElement}
         }
     })
 </script>
