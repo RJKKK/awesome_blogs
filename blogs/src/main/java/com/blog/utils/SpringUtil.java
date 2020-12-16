415624a4bd2cbbf45476b23fce314216aeab923f
qinshoumi <996489871@qq.com>
2020-12-16 22:18:18 +0800

    整合swagger2

---
+++ blogs/src/main/java/com/blog/utils/SpringUtil.java
@@ -0,0 +1,16 @@
+//package com.blog.utils;
+//
+//import org.apache.catalina.core.ApplicationContext;
+//
+//public class SpringUtil {
+//    private static ApplicationContext applicationContext;
+//
+//    private static byte environ = 0;
+//
+//    public static void setApplicationContext(ApplicationContext context){
+//        SpringUtil.applicationContext = context;
+//        //获取当前的系统环境
+//        applicationContext.getEnvionment
+//
+//    }
+//}
--- blogs/src/main/java/com/blog/vo/UserVo.java
+++ blogs/src/main/java/com/blog/vo/UserVo.java
@@ -1,6 +1,8 @@
 package com.blog.vo;
 
 import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,20 +10,32 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.util.Date;
 
+@ApiModel("用户视图类")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserVo implements Serializable {
+
     @TableId
+    @ApiModelProperty("用户ID")
     private int userId;
+    @ApiModelProperty("用户账号名")
     private String account;
+    @ApiModelProperty("用户名")
     private String username;
+    @ApiModelProperty("用户密码")
     private String password;
+    @ApiModelProperty("用户状态")
     private boolean status;
+    @ApiModelProperty("用户类型")
     private boolean type;
+    @ApiModelProperty("用户权限")
     private String auth;
+    @ApiModelProperty("用户创建时间")
     private Date createTime;
+    @ApiModelProperty("用户最后修改时间")
     private Date updateTime;
+    @ApiModelProperty("电话")
     private int phone;
 
 }
