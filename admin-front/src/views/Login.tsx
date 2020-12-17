f43853931352ed253001dacb8b51de6a9a07ad5a
RJKKK <shengunkun@gmail.com>
2020-12-18 00:45:20 +0800

    登录页面
    Signed-off-by: RJKKK <shengunkun@gmail.com>

--- admin-front/src/views/Login.tsx
+++ admin-front/src/views/Login.tsx
@@ -1,12 +1,11 @@
 import React, {useState} from 'react'
 import styled from "styled-components";
-import {Form,Input} from 'antd'
+import {Form, Input,Button} from 'antd'
 import useMyForm from "../hook/useMyForm";
 import {loginForm} from "../api/reqInterface";
-import {account} from "../untils/FormValidation";
-export default function Login() {
-    const Style = styled('div')`
- #login{
+import {account as rAccount, password as rPassword} from "../untils/FormValidation";
+const Style = styled('div')`
+ .login{
         margin:0;
         background-color: #29f1c3;
         display:flex;
@@ -16,55 +15,76 @@ export default function Login() {
         min-height:100vh;
     }
     .row{
-        width: 600px;
-        margin:.6rem 0;
-        position: relative;
+        width: 400px;
+        //margin:.6rem 0;
+        //position: relative;
         // display:flex;
         // justify-content:center;
         // align-items:center;
         // flex-direction:column;
         // min-height:100vh;
     }
-    .row input{
-        font-size:1rem;
-        border: 1px green solid;
-        border-radius:4px;
-        box-sizing:border-box;
-        padding:.8rem 1rem;
-        box-shadow:0px 1px 2px rgba(0,0,0,.25);
-        width: 100%;
-        outline:none;
-        // placeholder:' ';
-    }
-    .row label{
-        position:absolute;
-        left:0px;
-        padding-left:1rem;
-        padding-top:.8rem;
-        transition:all .3s ease-in-out;
-    }
-    .row input:focus + label{
-        transform:translatex(calc(-100% - 1rem));
-    }
-    .row input:not(:focus):not(:placeholder-shown) + label{
-        color:rgba(0,0,0,0)
-    }
+    //.row input{
+    //    font-size:1rem;
+    //    border: 1px green solid;
+    //    border-radius:4px;
+    //    box-sizing:border-box;
+    //    padding:.8rem 1rem;
+    //    box-shadow:0px 1px 2px rgba(0,0,0,.25);
+    //    width: 100%;
+    //    outline:none;
+    //    // placeholder:' ';
+    //}
+    //.row label{
+    //    position:absolute;
+    //    left:0px;
+    //    padding-left:1rem;
+    //    padding-top:.8rem;
+    //    transition:all .3s ease-in-out;
+    //}
+    //.row input:focus + label{
+    //    transform:translatex(calc(-100% - 1rem));
+    //}
+    //.row input:not(:focus):not(:placeholder-shown) + label{
+    //    color:rgba(0,0,0,0)
+    //}
 `
+export default function Login() {
+
+   const [password,setPassword] = useState<string>('')
+    const [account,setAccount] = useState<string>('')
     const [form] = Form.useForm()
-    // @ts-ignore
+    const toLogin = ()=>{
+       form.validateFields().then(()=>{
+           console.log('提交了')
+       }).catch(()=>{console.log('未通过验证')})
+    }
     return (
         <Style>
-            <Form id="login" form={form}>
+            <Form className="login" form={form}>
                 <h2>管理员登入</h2>
-                <Form.Item name={'account'} className="row"  >
-                    <Input type="text" id='username'/>
-                </Form.Item>
-                <label htmlFor="username">用户名</label>
-                <Form.Item name={'password'} className="row">
-                    <Input type="password" id='pwd'/>
-                </Form.Item>
-                <Form.Item><label htmlFor="pwd">密码</label></Form.Item>
+                <div className="row">
+                    <Form.Item name={'account'} rules={rAccount} label={'账号'}>
+                        <Input type="text" id='username'
+                               onChange={e=>setAccount(e.target.value)}
+                               value={account}/>
+                        {/*<label htmlFor="username">用户名</label>*/}
+                    </Form.Item>
 
+                </div>
+
+                <div className="row">
+                    <Form.Item name={'password'} className="row" rules={rPassword} label={'密码'}>
+                        <Input type="password" id='pwd'
+                               onChange={e=>setPassword(e.target.value)}
+                               value={password}
+                        />
+                        {/*<label htmlFor="pwd">密码</label>*/}
+                    </Form.Item>
+
+                </div>
+
+                <Button onClick={toLogin}>登录</Button>
             </Form>
         </Style>
     )
